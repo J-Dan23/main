bf554e2776cc557d30d094cc99c6945c23a81a8b
Janice Ong Yi Wen <55853397+J-Dan23@users.noreply.github.com>
2020-03-09 22:14:35 +0800

    fix import order and delete unused import statement

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,11 +1,10 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's remark in the remark book.
- * Guarantees: immutable; is valid as declared in {@link #isValidRemark(String)}
+ * Guarantees: immutable; is valid as declared in {@link /isValidRemark(String)}
  */
 public class Remark {
 
